6495a056ea3e9d2c7b6e2d21e9c9cf4e5bbc8f23
npicciano79 <npicciano79@gmail.com>
2021-10-20 20:15:49 -0400

    clock.py

--- clock.py
+++ clock.py
@@ -1,1 +1,20 @@
 #digital clock 
+from datetime import datetime
+import datetime
+from tkinter import *
+from tkinter.ttk import*
+
+
+def form():
+    
+
+
+
+def clock():
+
+
+
+
+
+if __name__=="__main__":
+    

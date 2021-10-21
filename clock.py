336aad03e8570ebb6960051bbda800b0f32afce7
npicciano79 <npicciano79@gmail.com>
2021-10-20 20:26:50 -0400

    clock.py

--- clock.py
+++ clock.py
@@ -1,20 +1,28 @@
 #digital clock 
+from _typeshed import Self
 from datetime import datetime
 import datetime
-from tkinter import *
-from tkinter.ttk import*
+import tkinter as tk
+
+
 
 
 def form():
-    
+    r=tk.Tk()
+    r.title('Test')
+    text=tk.Text(r,text="test",width=25)
+    text.pack()
+    r.mainloop()
 
 
 
-def clock():
+
+
 
 
 
 
 
 if __name__=="__main__":
+    form()
     

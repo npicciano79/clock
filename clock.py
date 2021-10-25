7c010ef65750d981c32c68c7f4395b1880e2ac95
npicciano79 <npicciano79@gmail.com>
2021-10-25 08:47:15 -0400

    clock.py

--- clock.py
+++ clock.py
@@ -2,17 +2,27 @@
 from datetime import datetime, time
 from time import gmtime
 import tkinter as tk
-from tkinter.constants import S
+#from tkinter.constants import S
+
 
 
 def clock():
+
+    
     now=datetime.now().strftime('%H:%M:%S %d-%m-%Y')
-    print(now)
+    #print(now)
 
 
     
     r=tk.Tk()
     r.title('Clock')
+    r.geometry("420x150")
+    text_font=("Helvetical", 30,'bold')
+    background='#add8e6'
+    cclock=tk.Label(r,text=now,bg=background, font=text_font)
+    cclock.grid(row=3,column=1)
+
+    
     r.mainloop()
 
 

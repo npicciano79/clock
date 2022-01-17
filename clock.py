a2270febf65839c23777034236eb5491a5e0b2bb
npicciano79 <npicciano79@gmail.com>
2022-01-17 12:43:39 -0500

    clock.py

--- clock.py
+++ clock.py
@@ -5,26 +5,28 @@ from datetime import datetime, time
 from time import gmtime
 import tkinter as tk
 
-
 r=tk.Tk()
 r.title('Clock')
 r.geometry("420x150")
-time_font=("calibri", 50,'bold')
-day_font=("calibri", 20)
+time_font=("ds-digital", 50,'bold')
+day_font=("ds-digital", 20)
 background='#000000'
-c_label=tk.Label(r,font=time_font,bg=background,foreground='#FFFF00')
+c_label=tk.Label(r,font=time_font,bg=background,foreground='#00FF00')
 c_label.place(relx=0.05, rely=0.05, relheight=0.6, relwidth=0.9)
-date_label=tk.Label(r,font=day_font,bg=background,foreground='#FFFF00')
+date_label=tk.Label(r,font=day_font,bg=background,foreground='#00FF00')
 date_label.place(relx=0.05, rely=0.65, relheight=0.2, relwidth=0.5)
-day_label=tk.Label(r,font=day_font,bg=background,foreground='#FFFF00')
+day_label=tk.Label(r,font=day_font,bg=background,foreground='#00FF00')
 day_label.place(relx=0.55, rely=0.65, relheight=0.2, relwidth=0.40)
 
 
+
+
 def clock():
-    
     now=datetime.now().strftime('%H:%M:%S %m/%d/%Y')
     time,date=now.split()
+    day_index=datetime.today().weekday()
     #print(f"date{date} time{time}")
+    days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
     #convert hour from military to standard
     n_hour=int(time.split(':')[0])
@@ -33,23 +35,25 @@ def clock():
         setting="AM"
     elif n_hour>12:
         n_hour=n_hour-12
-    
+
     min=time.split(':')[1]
     sec=time.split(':',2)[1]
-    
+
     t_now=str(n_hour)+":"+min+":"+sec+" "+setting
- 
+
     #print(f"time: {t_now} ")
     c_label.config(text=t_now)
-    c_label.after(10,clock)
-    date_label.config(text='test')
-    day_label.config(text='test')
+    
+    date_label.config(text=date)
+    day_label.config(text=days[day_index])
+    r.after(1000,clock)
 
 
 
-if __name__=="__main__":
-    clock()    
-    r.mainloop()
+
+
+clock()
+r.mainloop()
 
 
 
